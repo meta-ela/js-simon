2c1e36f522acbacb2f0e5ba2b2748e2ed3e45b7d
meta-ela <anxhelameta@live.it>
2022-05-31 19:17:59 +0200

    Adds compare function

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ const timerContainer = document.getElementById("timerContainer");
 let randomNumbers = [];
 // creo array vuoto dei numeri inseriti dall'utente
 let userNumbers = [];
+// array con i numeri indovinati
+let foundNumbers = [];
 
 
 // ciclo per creare i numeri random e pusharli nell'array randomNumbers
@@ -39,7 +41,7 @@ randomNumbersContainer.innerHTML = `Memorizza questi numeri: <br>
 
 
 // avviare un timer di 30 secondi
-let secondsRange = 30;
+let secondsRange = 10;
 
 // salvare in una variabile per fermare il countdown a 0
 const timerSecondi = setInterval(function () {
@@ -56,7 +58,7 @@ const timerSecondi = setInterval(function () {
     // quando var secondsRange e uguale a 0, si ferma il timer
     if (secondsRange === 0) {
         clearInterval(timerSecondi);
-        randomNumbersContainer.innerHTML = `Inserisci,in alto, i numeri memorizzati :)`;
+        randomNumbersContainer.innerHTML = `Inserisci, in alto, i numeri memorizzati :)`;
     }
 }, 1000);
 
@@ -72,7 +74,7 @@ setTimeout(function () {
     do {
         const askUser = prompt("inserisci, uno alla volta, i numeri appena visualizzati");
         // controllo se inserisce non un numero
-        if (isNaN(askUser)) {
+        if (isNaN(askUser || undefined)) {
             alert("inserisci un numero, non una lettera.")
         } else {
             userNumbers.push(askUser);
@@ -80,11 +82,11 @@ setTimeout(function () {
     } while (userNumbers.length < 5)
 
     console.log("Numeri utente: " + userNumbers);
-}, 31000);
+}, 11000);
 
 
 // funzione per controllare la sintassi del timer
-function syntaxController (text, lenght, placeholderChar) {
+/* function syntaxController (text, lenght, placeholderChar) {
     let result = text;
 
     while(result.lenght < lenght) {
@@ -92,4 +94,37 @@ function syntaxController (text, lenght, placeholderChar) {
     }
     
     return result;
+} */
+
+
+// comparazione array
+/* 
+- controllare l'array dell'utente
+- ciclare su ogni suo elemento
+- controllare se quell'elemento è presente nell'array random
+- pushare il numero array utente in un array di trovati se corrisponde al random
+- comparare lunghezza array trovati con array random come prova finale.
+*/
+
+//?????????????????????????????????????
+function compareArrays () {
+    // compara la lunghezza dei 2 array, se diversi allora false a prescindere
+    if (userNumbers.length != randomNumbers.length) {
+        return false;
+        alert("Non hai inserito tutti e 5 i numeri richiesti.");
+    } else {
+        let result = false;
+
+        for (let i = 0; i < userNumbers.length; i++) {
+            if (userNumbers[i] != randomNumbers[i]) {
+                return false;
+            } else {
+                result = true;
+            }
+        }
+        return result;
+    }
+    
 }
+
+let result = compareArrays(userNumbers, randomNumbers);
